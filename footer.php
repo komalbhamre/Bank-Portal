996602302651977f903ee58184ebdac7839de6c8
komalbhamre <bhamrekomal2003@gmail.com>
2024-11-09 12:07:42 +0530

    first commit

---
+++ footer.php
@@ -0,0 +1,14 @@
+<!DOCTYPEhtml>
+<html>
+  <head>
+    <link rel="stylesheet" href="styles1.css">
+  </head>
+  <body>
+<!-- Footer -->
+<footer class="footer">
+              <p>2024@ Bank Portal</p>
+            </footer>
+<!-- End Footer -->
+  </body>
+</html>
+
---
+++ staff/footer.php
@@ -0,0 +1,19 @@
+<!DOCTYPEhtml>
+<html>
+  <head>
+  <style>
+        .footer{
+    text-align: center;
+    margin-top: 50px;
+    margin-left: 160px;
+}
+</style>
+  </head>
+  <body>
+<!-- Footer -->
+<footer class="footer">
+              <p>2024@ Bank Portal</p>
+            </footer>
+<!-- End Footer -->
+  </body>
+</html>
